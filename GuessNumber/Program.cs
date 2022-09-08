8ac411ff18c735ea928c348c76c043c69f2b37d4
sergeevich ruslan <abishev2012@gmail.com>
2022-09-08 22:02:11 +0500

    программа полностью переделана

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -5,59 +5,70 @@ using System.Text;
 
 namespace GuessNumber
 {
-    class Program
+    class Ugaday4islo
+
     {
-        static void Main(string[] args)
+        // функция создания случайного числа в диапозоне от 1 до как получит через параметр
+        static int GetRandom(int rNum)
         {
-            Console.WriteLine("1. Легко (0 - 50)");
-            Console.WriteLine("2. Средне (0 - 100)");
-            Console.WriteLine("3. Сложно (0 - 200)");
-
-            Console.WriteLine("Выбор режима (напишите число): ");
-            int regim = 0;
-
-            Random ram = new Random();
-            int num = 0;
-            while (true)
-            {
-                regim = Convert.ToInt32(Console.ReadLine());
-                if (regim == 1)
-                {
-                    num = ram.Next(0, 50);
-                    break;
-                }
-                else if (regim == 2)
-                {
-                    num = ram.Next(0, 100);
-                    break;
-                }
-                else if (regim == 3)
-                {
-                    num = ram.Next(0, 200);
-                    break;
-                }
-                else
-                    Console.WriteLine("Введено недопустимое число. Введите 1, 2, либо 3.");
-            }
-
-            Console.WriteLine("Я загадал число. Угадай его.");
-
-            int input = 0;
-            while (true)
-            {
-                input = Convert.ToInt32(Console.ReadLine());
-                if (num > input)
-                    Console.WriteLine("Моё число больше. :P");
-                else if (num < input)
-                    Console.WriteLine("Моё число меньше. :P");
-                else
-                {
-                    Console.WriteLine("Поздравляем, Вы угадали! :) ");
-                    break;
-                }
-
-            }
+            // создание генератора случайных чисел
+            Random rnd = new Random();
+            // создание случайного числа 
+            int value = rnd.Next(1, rNum);
+            // возврат из функции созданого числа
+            return value;
         }
+        
+        public static void Main(string[] args)
+        {
+            int iNum = 0; // инициализация числа 
+            int pNum = 100; // инициализация числа для диапазона случайных чисел
+            int rNum = GetRandom(pNum); // инициализация случайного числа из диапазона
+            //окрас консоли .приветсвие и запрос имени
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.Write("Здравствуйте, это игра под названием <Угадай число>! \nКак вас зовут? ");
+            string name = Console.ReadLine();
+            for (int i = 1; i < 11; i++) // цикл от от 1 до 11. Это 10 попыток
+            {
+                // приветсвенное сообщение угадать число
+                Console.Write(name + $" Угадай число от 1 до {pNum}! У тебя есть 10 попыток - Попытка {i}: \n");
+                string sNum = Console.ReadLine(); // чтение введенного числа с клавиатуры
+                // условие проверки ввода. число или буква
+                if (int.TryParse(sNum, out iNum) && iNum <= pNum)
+                {
+                    if (iNum == rNum) // условие определения правильности числа
+                    {
+                        // вывод сообщения о правильности числа и прекращение цикла
+                        Console.WriteLine("Вы угадали! Поздравляю!! Правильное число " + rNum);
+                        break;
+                    }
+                    // услови если число меньше загаданного
+                    if (iNum < rNum)
+                    {
+                        Console.WriteLine("Ваше число " + iNum + " а должно быть больше");
+                    }
+                    // условие если число больше загаданного
+                    if (iNum > rNum)
+                        Console.WriteLine("Ваше число " + iNum + "а должно быть меньше");
+
+                }
+                else
+                {
+                    // если введено не число или оно не соответствует диапазону pNum
+                    Console.WriteLine("Нужно вводить число от 1 до " + pNum);
+                }
+
+            }
+            // сообщение от том что число не угадано
+            if (iNum != rNum)
+                Console.WriteLine("Вы не угадали число!");
+            // ожидания ввода с клавиатуры
+            Console.ReadKey();
+        }
+
+
     }
 }
 
